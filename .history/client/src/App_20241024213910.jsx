cb4a6bcbcaf811fa903c319b43cb152980390629
thiraphongo <jackteerapong2017@gmail.com>
2024-10-24 21:49:12 +0700

    first commit

---
+++ .history/client/src/App_20241024213910.jsx
@@ -0,0 +1,8 @@
+
+const App = () => {
+  return (
+    <h1 className="text-red">App</h1>
+  )
+}
+
+export default App
---
+++ .history/client/src/App_20241024214456.jsx
@@ -0,0 +1,8 @@
+
+const App = () => {
+  return (
+    <h1 className="text-red-500 flex-col">App</h1>
+  )
+}
+
+export default App
